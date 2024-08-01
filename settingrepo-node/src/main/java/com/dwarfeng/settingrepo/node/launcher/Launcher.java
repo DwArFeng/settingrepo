659ffceeaeaf18be985553b16f06a7ca8d51e5c8
DwArFeng <915724865@qq.com>
2024-08-02 00:22:33 +0800

    启动器优化
    将入口方法中完整独立的功能封装在子方法中，使入口方法代码结构更加清晰
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- settingrepo-node/src/main/java/com/dwarfeng/settingrepo/node/launcher/Launcher.java
+++ settingrepo-node/src/main/java/com/dwarfeng/settingrepo/node/launcher/Launcher.java
@@ -7,6 +7,7 @@ import com.dwarfeng.springterminator.sdk.util.ApplicationUtil;
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.Date;
@@ -27,46 +28,62 @@ public class Launcher {
                 "file:opt/opt*.xml",
                 "file:optext/opt*.xml"
         }, ctx -> {
-            LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
-            //判断是否重置格式化器。
-            if (launcherSettingHandler.isResetFormatterSupport()) {
-                LOGGER.info("重置格式化器支持...");
-                FormatterSupportMaintainService maintainService = ctx.getBean(FormatterSupportMaintainService.class);
-                try {
-                    maintainService.reset();
-                } catch (ServiceException e) {
-                    LOGGER.warn("格式化器支持重置失败，异常信息如下", e);
-                }
-            }
+            // 根据启动器设置处理器的设置，选择性重置格式化器。
+            mayResetFormatter(ctx);
 
-            // 拿出程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
-            ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
-
-            // 处理重置处理器的启动选项。
-            ResetQosService resetQosService = ctx.getBean(ResetQosService.class);
-            // 重置处理器是否启动重置服务。
-            long startResetDelay = launcherSettingHandler.getStartResetDelay();
-            if (startResetDelay == 0) {
-                LOGGER.info("立即启动重置服务...");
-                try {
-                    resetQosService.start();
-                } catch (ServiceException e) {
-                    LOGGER.error("无法启动重置服务，异常原因如下", e);
-                }
-            } else if (startResetDelay > 0) {
-                LOGGER.info(startResetDelay + " 毫秒后启动重置服务...");
-                scheduler.schedule(
-                        () -> {
-                            LOGGER.info("启动重置服务...");
-                            try {
-                                resetQosService.start();
-                            } catch (ServiceException e) {
-                                LOGGER.error("无法启动重置服务，异常原因如下", e);
-                            }
-                        },
-                        new Date(System.currentTimeMillis() + startResetDelay)
-                );
-            }
+            // 根据启动器设置处理器的设置，选择性启动重置服务。
+            mayStartReset(ctx);
         });
+    }
+
+    private static void mayResetFormatter(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 判断是否重置格式化器支持。
+        if (launcherSettingHandler.isResetFormatterSupport()) {
+            LOGGER.info("重置格式化器支持...");
+            FormatterSupportMaintainService maintainService = ctx.getBean(FormatterSupportMaintainService.class);
+            try {
+                maintainService.reset();
+            } catch (ServiceException e) {
+                LOGGER.warn("格式化器支持重置失败，异常信息如下", e);
+            }
+        }
+    }
+
+    private static void mayStartReset(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 获取程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
+        ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
+
+        // 处理重置处理器的启动选项。
+        ResetQosService resetQosService = ctx.getBean(ResetQosService.class);
+
+        // 重置处理器是否启动重置服务。
+        long startResetDelay = launcherSettingHandler.getStartResetDelay();
+        if (startResetDelay == 0) {
+            LOGGER.info("立即启动重置服务...");
+            try {
+                resetQosService.start();
+            } catch (ServiceException e) {
+                LOGGER.error("无法启动重置服务，异常原因如下", e);
+            }
+        } else if (startResetDelay > 0) {
+            LOGGER.info("{} 毫秒后启动重置服务...", startResetDelay);
+            scheduler.schedule(
+                    () -> {
+                        LOGGER.info("启动重置服务...");
+                        try {
+                            resetQosService.start();
+                        } catch (ServiceException e) {
+                            LOGGER.error("无法启动重置服务，异常原因如下", e);
+                        }
+                    },
+                    new Date(System.currentTimeMillis() + startResetDelay)
+            );
+        }
     }
 }
